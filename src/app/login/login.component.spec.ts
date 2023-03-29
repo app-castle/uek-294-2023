aa8c2022c75168d17f83a1a10af3619ef16f570c
Markus Chiarot <cmarkus@hotmail.ch>
2023-03-29 09:40:29 +0200

    unit test for onLogin

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,7 +1,11 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgForm } from '@angular/forms';
+import { of } from 'rxjs';
 
 import { LoginComponent } from './login.component';
+import { LoginResponse } from './user';
+import { UserService } from './user.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -17,7 +21,76 @@ describe('LoginComponent', () => {
     fixture.detectChanges();
   });
 
+  beforeEach(() => {
+    sessionStorage.removeItem('auth-token');
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('will break if the form is invalid', () => {
+    // arrange
+    const form = {
+      invalid: true,
+    } as NgForm;
+
+    const service = TestBed.inject(UserService);
+    spyOn(service, 'login');
+
+    // act
+    component.onLogin(form);
+
+    // assert
+    expect(service.login).not.toHaveBeenCalled();
+  });
+
+  it('will execute login if form is valid', () => {
+    // arrange
+    const form = {
+      invalid: false,
+      value: {
+        email: 'example@test.com',
+        password: 'blablapassword',
+      },
+    } as NgForm;
+
+    const service = TestBed.inject(UserService);
+    spyOn(service, 'login').and.callThrough();
+
+    // act
+    component.onLogin(form);
+
+    // assert
+    expect(service.login).toHaveBeenCalled();
+  });
+
+  it('has an auth token in the session storage after login', () => {
+    // arrange
+    const form = {
+      invalid: false,
+      value: {
+        email: 'example@test.com',
+        password: 'blablapassword',
+      },
+    } as NgForm;
+
+    const service = TestBed.inject(UserService);
+    spyOn(service, 'login').and.returnValue(
+      of({
+        accessToken: 'awesome-token-bla-bla',
+        user: {
+          email: 'example@test.com',
+          id: 1,
+        },
+      } as LoginResponse)
+    );
+
+    // act
+    component.onLogin(form);
+
+    // assert
+    const token = sessionStorage.getItem('auth-token');
+    expect(token).toBe('awesome-token-bla-bla');
   });
 });
